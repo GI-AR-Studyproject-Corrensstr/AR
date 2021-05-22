86f4ec7ced3003294d5d723390a88784c958030f
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2021-05-22 12:20:52 +0200

    change models from menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,14 @@
-const sceneEl = document.getElementById('scene');
-const el = document.getElementById('a-entity')
-var rotationFactor = 5;
-
-let isMarkerVisible;
-
-sceneEl.addEventListener("markerFound", (e) => {
-    isMarkerVisible = true;
+$(document).ready(function() {
+    $('#iframe').on( 'load', function() {
+        var iframe = document.getElementById("iframe");
+        var gltfentity = iframe.contentWindow.document.getElementById("a-entity");
+        $('#car').click((e) => {
+            gltfentity.setAttribute('gltf-model', '/gltf/car_model/scene.gltf');
+            gltfentity.setAttribute('scale', '0.8 0.8 0.8');
+        })
+        $('#truck').click((e) => {
+            gltfentity.setAttribute('gltf-model', '/gltf/truck_model/scene.gltf');
+            gltfentity.setAttribute('scale', '0.3 0.3 0.3');
+        })
+    });
 });
-
-sceneEl.addEventListener("markerLost", (e) => {
-    isMarkerVisible = false;
-});
-
-sceneEl.addEventListener('onefingermove', (e) => {
-    if (isMarkerVisible) {
-        el.object3D.rotation.y +=
-            e.detail.positionChange.x * rotationFactor;
-
-        el.object3D.rotation.x +=
-            e.detail.positionChange.y * rotationFactor;
-    }
-})
-
-sceneEl.addEventListener('twofingermove', (e) => {
-    if (isMarkerVisible) {
-        this.scaleFactor *=
-            1 + e.detail.spreadChange / e.detail.startSpread;
-    }
-})
